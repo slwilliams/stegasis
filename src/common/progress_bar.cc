72c8d29adc6067e912b2d67b32ca76a89522d04c
slwilliams <slw69@cam.ac.uk>
2014-11-06 23:28:55 +0000

    Minor style changes to progress bar.

--- src/common/progress_bar.cc
+++ src/common/progress_bar.cc
@@ -2,25 +2,24 @@
 
 #include "progress_bar.h"
 
-// Process has done i out of n rounds,
-// and we want a bar of width w.
+// Taken from rosshemsley.co.uk/2011/02/creating-a-progress-bar-in-c-or-any-other-console-app/
+// Slightly modified
+
+// Process has done i out of n rounds, with width w
 void loadBar(int x, int n, int w) {
-    // Calculuate the ratio of complete-to-incomplete.
-    float ratio = x/(float)n;
-    int   c     = ratio * w;
+    float ratio = x / (float)n;
+    int c = ratio * w;
  
-    // Show the percentage complete.
-    printf("%3d%% [", (int)(ratio*100) );
+    printf("%3d%% [", (int)(ratio * 100));
  
-    // Show the load bar.
-    for (int x=0; x<c; x++)
+    int i = 0;
+    for (i = 0; i < c; i ++) {
        printf("=");
- 
-    for (int x=c; x<w; x++)
+    }
+    for (i = c; i < w; i ++) {
        printf(" ");
+    }
  
-    // ANSI Control codes to go back to the
-    // previous line and clear it.
     if (x != n) {
       printf("]\n\033[F\033[J");
     } else {
