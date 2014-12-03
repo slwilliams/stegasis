29ce3eb7a1629fdb8f011433d4e0f99b7cf8e99f
slwilliams <slw69@cam.ac.uk>
2014-12-03 21:51:04 +0000

    Work on stegasis site.

--- src/site/app.js
+++ src/site/app.js
@@ -62,5 +62,3 @@ var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);           
 }); 
 
-
-module.exports = app;
--- src/site/routes/index.js
+++ src/site/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
 var router = express.Router();
+var uuid = require('node-uuid');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  // Set a unique cookie if we haven't seen this person before
+  if (!req.cookies.id) {
+    res.cookie('id', uuid.v4(), { maxAge: 31536000730 });
+  }
+  res.render('index', { title: 'Stegasis' });
+});
+
+router.post('/submit/:img', function(req, res) {
+  if (!req.cookies.id) { 
+    // We haven't seen this person before...
+    res.redirect("/");
+  }
+  console.log("img: " + req.param("img"));
+  console.log("cookie: " + req.cookies.id);
+  MongoClient.connect("mongodb://localhost:27017/stegasis", function(err, db) {
+    if(!err) {
+      console.log("We are connected");
+      res.redirect("/");
+    }
+  }); 
 });
 
 module.exports = router;
