3ab1762c6eeee2a6b5ef7d845e4fbc87ce282f9c
slwilliams <slw69@cam.ac.uk>
2014-12-28 00:29:05 +0000

    Slight clean up to lsb_alg.cc.

--- src/steg/lsb_algorithm.cc
+++ src/steg/lsb_algorithm.cc
@@ -7,13 +7,10 @@ class LSBAlgorithm : public SteganographicAlgorithm {
   public:
     virtual void embed(Chunk *c, char *data, int dataBytes, int offset) {
       char *frame = c->getFrameData(0);
-      int i = 0;
-      int j = 0;
       int frameByte = offset;
-      char mask = 1;
-      for (i = 0; i < dataBytes; i ++) {
-        for (j = 7; j >= 0; j --) {
-          if ((((mask << j) & data[i]) >> j) == 1) {
+      for (int i = 0; i < dataBytes; i ++) {
+        for (int j = 7; j >= 0; j --) {
+          if ((((1 << j) & data[i]) >> j) == 1) {
             frame[frameByte] |= 1;
           } else {
             frame[frameByte] &= ~1;
@@ -24,12 +21,10 @@ class LSBAlgorithm : public SteganographicAlgorithm {
     };
     virtual void extract(Chunk *c, char *output, int dataBytes, int offset) {
       char *frame = c->getFrameData(0);
-      int i = 0;
-      int j = 0;
       int frameByte = offset;
-      for (i = 0; i < dataBytes; i ++) {
+      for (int i = 0; i < dataBytes; i ++) {
         output[i] = 0;
-        for (j = 7; j >= 0; j --) {
+        for (int j = 7; j >= 0; j --) {
           output[i] |= ((frame[frameByte] & 1) << j);
           frameByte ++;
         }
